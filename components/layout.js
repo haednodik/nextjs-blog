b8b713e1c9e8ef2a6560ec27cb902c4969989597
haednodik <haednodik@gmail.com>
2026-01-18 23:28:26 -0500

    Update site title and meta description

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
  
 const name = 'Chheany Mok';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'CMok Website';
  
 export default function Layout({ children, home }) {
   return (
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="My personal website using Next.js"
         />
         <meta
           property="og:image"
